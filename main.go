62ccc1a1bc360fac951088d0909140449751c720
TamHT <tamht3698@gmail.com>
2020-12-04 00:07:49 +0700

    add embed model

--- main.go
+++ main.go
@@ -7,11 +7,16 @@ import (
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
 )
+
+var prefix string
 
 func main() {
 
+	godotenv.Load()
 	token := os.Getenv("BOT_TOKEN")
+	prefix = os.Getenv("PREFIX")
 
 	dg, err := discordgo.New("Bot " + token)
 
@@ -48,9 +53,24 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if strings.HasPrefix(m.Content, "~") {
-		if m.Content == "cuc" {
-			s.ChannelMessageSend(m.ChannelID, "cac")
+	if strings.HasPrefix(m.Content, prefix) {
+		content := m.Content[len(prefix):len(m.Content)]
+
+		args := strings.Fields(content)
+		if len(args) != 2 {
+			s.ChannelMessageSend(m.ChannelID, "Lệnh sai thằng ngu ạ!")
+			return
+		}
+		cmd := strings.ToLower(args[0])
+
+		if cmd == "check" {
+			// r, _ := http.PostForm("https://trade.vndirect.com.vn/chung-khoan/danh-muc",
+			// 	url.Values{"searchMarketStatisticsView.symbol": {args[1]}})
+			// bytes, _ := ioutil.ReadAll(r.Body)
+
+			msg := &discordgo.MessageEmbed{Title: "Bảng giá mã " + strings.ToUpper(args[1])}
+
+			s.ChannelMessageSendEmbed(m.ChannelID, msg)
 		}
 	}
 
---
+++ models/embed.go
@@ -0,0 +1,77 @@
+package models
+
+import "github.com/bwmarrin/discordgo"
+
+//Embed ...
+type Embed struct {
+	*discordgo.MessageEmbed
+}
+
+// Constants for message embed character limits
+const (
+	EmbedLimitTitle       = 256
+	EmbedLimitDescription = 2048
+	EmbedLimitFieldValue  = 1024
+	EmbedLimitFieldName   = 256
+	EmbedLimitField       = 25
+	EmbedLimitFooter      = 2048
+	EmbedLimit            = 4000
+)
+
+//NewEmbed returns a new embed object
+func NewEmbed() *Embed {
+	return &Embed{&discordgo.MessageEmbed{}}
+}
+
+//SetTitle [name]
+func (e *Embed) SetTitle(name string) *Embed {
+	e.Title = name
+	return e
+}
+
+//SetDescription [desc]
+func (e *Embed) SetDescription(description string) *Embed {
+	if len(description) > EmbedLimitDescription {
+		description = description[:EmbedLimitDescription]
+	}
+	e.Description = description
+	return e
+}
+
+//AddField [name] [value]
+func (e *Embed) AddField(name, value string) *Embed {
+	if len(value) > EmbedLimitFieldValue {
+		value = value[:EmbedLimitFieldValue]
+	}
+
+	if len(name) > EmbedLimitFieldName {
+		name = name[:EmbedLimitFieldName]
+	}
+
+	e.Fields = append(e.Fields, &discordgo.MessageEmbedField{
+		Name:  name,
+		Value: value,
+	})
+
+	return e
+}
+
+//SetURL ...
+func (e *Embed) SetURL(URL string) *Embed {
+	e.URL = URL
+	return e
+}
+
+//SetColor ...
+func (e *Embed) SetColor(clr int) *Embed {
+	e.Color = clr
+	return e
+}
+
+// InlineAllFields sets all fields in the embed to be inline
+func (e *Embed) InlineAllFields() *Embed {
+	for _, v := range e.Fields {
+		v.Inline = true
+	}
+	return e
+}
